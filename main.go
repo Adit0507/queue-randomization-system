11febd16081912e9604177a2548435f4da62d249
Adit0507 <adi05072002@gmail.com>
2025-04-24 18:16:48 +0530

    fixed the routing for pages and serving the data

--- main.go
+++ main.go
@@ -11,9 +11,10 @@ func main() {
 	http.HandleFunc("/join", joinHandler)
 	http.HandleFunc("/status", statusHandler)
 	http.HandleFunc("/start", startHandler)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./templates/index.html")
-	})
+	http.HandleFunc("/", serveIndex	)
+
+	http.HandleFunc("/users", usersPageHandler)
+	http.HandleFunc("/users-data", usersDataHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
@@ -22,6 +23,34 @@ func main() {
 func joinHandler(w http.ResponseWriter, r *http.Request) {
 	userID := q.AddUser()
 	json.NewEncoder(w).Encode(map[string]string{"user_id": userID})
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.ServeFile(w, r, "./templates/index.html")
+		return
+	}
+	if r.URL.Path == "/users" {
+		http.ServeFile(w, r, "./templates/users.html")
+		return
+	}
+	http.NotFound(w, r)
+}
+
+func usersPageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./templates/users.html")
+}
+
+func usersDataHandler(w http.ResponseWriter, r *http.Request) {
+	q.Mu.Lock()
+	defer q.Mu.Unlock()
+
+	var users []*User
+	for _, u := range q.Users {
+		users = append(users, u)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
 
 // returns user status
@@ -40,7 +69,7 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-// triggers queue randomization and processing 
+// triggers queue randomization and processing
 func startHandler(w http.ResponseWriter, r *http.Request) {
 	go q.ProcessQueues()
 	w.Write([]byte("Queue Started!!"))
